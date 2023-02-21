be7853efa1c99ea05c4d5d591bfa531d7d380136
ctwh0374 <ctwh0374@mail4.doshisha.ac.jp>
2023-02-21 07:47:00 +0000

    update index.js, setValues.py

--- setValues.py
+++ setValues.py
@@ -2,10 +2,10 @@ import eel
 import tkinter
 import tkinter.filedialog as filedialog
 
-import pyvisa
-from measure import Measure
-import numpy as np
-import pandas as pd
+# import pyvisa
+# from measure import Measure
+# import numpy as np
+# import pandas as pd
 
 
 # 辞書定義
@@ -13,7 +13,6 @@ setValues = {"lowSpeed": "", "highSpeed": "", "3dgpib": "", "set1stAxis": "","se
              "1stAxisPoint": "", "2ndAxisPoint": "", "3rdAxisPoint": "", "measure1": "", "measure2": "", "measure3": "", "measure4": "", "oscillogpib": ""}
 
 #stop_value=1の時「一時停止」、2の時「終了」
-global stop_value
 stop_value = 0
 
 # 新規ファイルの保存場所指定
@@ -36,34 +35,45 @@ def send_data(arg=[]):
         setValues[i] = arg[n]
         n = n+1
     print(setValues)  # 確認用
+    global stop_value
     print(stop_value)
-    
    
-    rm = pyvisa.ResourceManager()
-    visa_list = rm.list_resources()  
+    # rm = pyvisa.ResourceManager()
+    # visa_list = rm.list_resources()  
 
-    test = Measure(setValues,visa_list)
+    # test = Measure(setValues,visa_list)
     
     
-    data = test.measure_plane()
+    # data = test.measure_plane()
     
     
-    np.savetxt(file_path,data,delimiter=',') # データ保存
+    # np.savetxt(file_path,data,delimiter=',') # データ保存
     
     #現在位置の出力(テスト)
-    
     eel.change_current_point(1,5)
 
-    return "finish"  # UIに"finish"を返す。  
+    if stop_value==1:
+        return "suspending"
+    else:
+        return "finish"  # UIに"finish"を返す。  
+        
+# リセット
+@eel.expose
+def reset():
+    global stop_value #グローバル変数更新
+    stop_value=0
+    print(stop_value)
     
 # 一時停止
 @eel.expose
 def suspend():
+    global stop_value #グローバル変数更新
     stop_value=1
     print(stop_value)
     
 # ストップ
 @eel.expose
 def stop():
+    global stop_value#グローバル変数更新
     stop_value=2
     print(stop_value)
